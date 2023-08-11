0dbd15199b7ebc0dc082389aa141f48946b15991
514sid <userfind514@gmail.com>
2023-08-11 18:49:17 +0400

    add NumberValidator class with tests

---
+++ src/NumberValidator.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Num;
+
+class NumberValidator
+{
+    const POINT = '.';
+    const COMMA = ',';
+
+    public static function canBeInteger(string $input): bool
+    {
+        $cleanedInput = preg_replace("/[^\d,.]/", "", $input);
+
+        $dotCount = substr_count($cleanedInput, self::POINT);
+        $commaCount = substr_count($cleanedInput, self::COMMA);
+
+        if ($dotCount + $commaCount === 1) {
+            $separatorPosition = $dotCount === 1 ? strpos($cleanedInput, self::POINT) : strpos($cleanedInput, self::COMMA);
+
+            if (strlen(substr($cleanedInput, $separatorPosition + 1)) !== 3) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ tests/NumberValidatorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Num\Tests;
+
+use Num\NumberValidator;
+use PHPUnit\Framework\TestCase;
+
+class NumberValidatorTest extends TestCase
+{
+    public function testValidIntegerNumbers()
+    {
+        $this->assertTrue(NumberValidator::canBeInteger('123'));
+        $this->assertTrue(NumberValidator::canBeInteger('1,234'));
+        $this->assertTrue(NumberValidator::canBeInteger('1.234'));
+        $this->assertTrue(NumberValidator::canBeInteger('1,234,567'));
+    }
+
+    public function testInvalidIntegerNumbers()
+    {
+        $this->assertFalse(NumberValidator::canBeInteger('1,23'));
+        $this->assertFalse(NumberValidator::canBeInteger('1.2'));
+        $this->assertFalse(NumberValidator::canBeInteger('1.2345'));
+    }
+}
