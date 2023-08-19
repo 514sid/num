2e4c6d4aaf175db5c543d44c5524cf926f705631
514sid <140138716+514sid@users.noreply.github.com>
2023-08-19 08:23:53 +0400

    refactor tests

--- tests/NumTest.php
+++ tests/NumTest.php
@@ -8,72 +8,77 @@ use PHPUnit\Framework\TestCase;
 
 class NumTest extends TestCase
 {
-    public function testConversionOfNumberWithPointDecimalSeparatorToFloat()
+    public static function conversionData(): array
     {
-        $this->assertSame(123.0, Num::float('123', DecimalSeparator::POINT));
-        $this->assertSame(1234567.89, Num::float('1,234,567.89', DecimalSeparator::POINT));
-        $this->assertSame(1234567.0, Num::float('1,234,567', DecimalSeparator::POINT));
-        $this->assertSame(1234567.89, Num::float('1 234 567.89', DecimalSeparator::POINT));
-        $this->assertSame(1234567.89, Num::float('123,4567.89', DecimalSeparator::POINT));
-        $this->assertSame(1234567.89, Num::float('1\'234\'567.89', DecimalSeparator::POINT));
-        $this->assertSame(-1234567.89, Num::float('-1\'234\'567.89', DecimalSeparator::POINT));
-        $this->assertSame(12345.0, Num::float('12,345', DecimalSeparator::POINT));
-        $this->assertSame(123456.0, Num::float('12,3456', DecimalSeparator::POINT));
-        $this->assertSame(3.14159265359, Num::float('3.14159265359', DecimalSeparator::POINT));
-        $this->assertSame(12.30, Num::float('$12.30', DecimalSeparator::POINT));
-        $this->assertSame(-12.30, Num::float('-$12.30', DecimalSeparator::POINT));
-        $this->assertSame(0.0, Num::float('text', DecimalSeparator::POINT));
-        $this->assertSame(0.12, Num::float('.12', DecimalSeparator::POINT));
-        $this->assertSame(-123.45, Num::float('-123.45', DecimalSeparator::POINT));
-        $this->assertSame(123.0, Num::float(123));
+        return [
+            'Guesser' => [
+                'testCases' => [
+                    ['123', 123.0, 123],
+                    ['-123', -123.0, -123],
+                    ['12.34567', 12.34567, 12],
+                    ['12.34', 12.34, 12],
+                    ['12.345', 12345.0, 12345],
+                    ['-12.345', -12345.0, -12345],
+                    ['text', 0.0, 0],
+                    ['.12', 0.12, 0],
+                    [123.45, 123.45, 123],
+                    [123, 123.0, 123],
+                ],
+                'separator' => null,
+            ],
+
+            'Point Separator' => [
+                'testCases' => [
+                    ['123', 123.0, 123],
+                    ['1,234,567.89', 1234567.89, 1234567],
+                    ['1,234,567', 1234567.0, 1234567],
+                    ['1 234 567.89', 1234567.89, 1234567],
+                    ['123,4567.89', 1234567.89, 1234567],
+                    ['1\'234\'567.89', 1234567.89, 1234567],
+                    ['-1\'234\'567.89', -1234567.89, -1234567],
+                    ['12,345', 12345.0, 12345],
+                    ['12,3456', 123456.0, 123456],
+                    ['3.14159265359', 3.14159265359, 3],
+                    ['$12.30', 12.30, 12],
+                    ['-12.30', -12.30, -12],
+                    ['text', 0.0, 0],
+                    ['.12', 0.12, 0],
+                    ['-123.45', -123.45, -123],
+                    [123, 123.0, 123],
+                ],
+                'separator' => DecimalSeparator::POINT,
+            ],
+
+            'Comma Separator' => [
+                'testCases' => [
+                    ['123', 123.0, 123],
+                    ['1 234 567,89', 1234567.89, 1234567],
+                    ['1.234.567,89', 1234567.89, 1234567],
+                    ['1\'234\'567,89', 1234567.89, 1234567],
+                    ['12,345', 12.345, 12],
+                    ['-12,345', -12.345, -12],
+                    ['3,14159265359', 3.14159265359, 3],
+                    ['12.34567', 1234567.0, 1234567],
+                    ['12.34', 1234.0, 1234],
+                    ['12.345', 12345.0, 12345],
+                    ['-12.345', -12345.0, -12345],
+                    ['text', 0.0, 0],
+                    [',12', 0.12, 0],
+                ],
+                'separator' => DecimalSeparator::COMMA,
+            ],
+        ];
     }
 
-    public function testConversionOfNumberWithPointDecimalSeparatorToInteger()
+    /**
+     * @dataProvider conversionData
+     */
+    public function testConversion($testCases, $separator)
     {
-        $this->assertSame(123, Num::int('123', DecimalSeparator::POINT));
-        $this->assertSame(-123, Num::int('-123', DecimalSeparator::POINT));
-        $this->assertSame(1234567, Num::int('1,234,567.89', DecimalSeparator::POINT));
-        $this->assertSame(1234567, Num::int('1,234,567', DecimalSeparator::POINT));
-        $this->assertSame(1234567, Num::int('1 234 567.89', DecimalSeparator::POINT));
-        $this->assertSame(1234567, Num::int('123,4567.89', DecimalSeparator::POINT));
-        $this->assertSame(1234567, Num::int('1\'234\'567.89', DecimalSeparator::POINT));
-        $this->assertSame(12345, Num::int('12,345', DecimalSeparator::POINT));
-        $this->assertSame(123456, Num::int('12,3456', DecimalSeparator::POINT));
-        $this->assertSame(12, Num::int('12.34567'));
-        $this->assertSame(12, Num::int('12,34567'));
-        $this->assertSame(12, Num::int('$12.30', DecimalSeparator::POINT));
-        $this->assertSame(-12, Num::int('-$12.30', DecimalSeparator::POINT));
-        $this->assertSame(12, Num::int('$-12.30', DecimalSeparator::POINT));
-        $this->assertSame(0, Num::int('text', DecimalSeparator::POINT));
-        $this->assertSame(0, Num::int('.12', DecimalSeparator::POINT));
-        $this->assertSame(123, Num::int(123.45));
-    }
-
-    public function testConversionOfNumberWithCommaDecimalSeparatorToFloat()
-    {
-        $this->assertSame(123.0, Num::float('123', DecimalSeparator::COMMA));
-        $this->assertSame(1234567.89, Num::float('1 234 567,89', DecimalSeparator::COMMA));
-        $this->assertSame(1234567.89, Num::float('1.234.567,89', DecimalSeparator::COMMA));
-        $this->assertSame(1234567.89, Num::float('1\'234\'567,89', DecimalSeparator::COMMA));
-        $this->assertSame(12.345, Num::float('12,345', DecimalSeparator::COMMA));
-        $this->assertSame(-12.345, Num::float('-12,345', DecimalSeparator::COMMA));
-        $this->assertSame(3.14159265359, Num::float('3,14159265359', DecimalSeparator::COMMA));
-        $this->assertSame(12.34567, Num::float('12.34567'));
-        $this->assertSame(12.34, Num::float('12.34'));
-        $this->assertSame(12345.0, Num::float('12.345'));
-        $this->assertSame(-12345.0, Num::float('-12.345'));
-        $this->assertSame(0.0, Num::float('text', DecimalSeparator::COMMA));
-        $this->assertSame(0.12, Num::float(',12'));
-    }
-
-    public function testConversionOfNumberWithCommaDecimalSeparatorToInteger()
-    {
-        $this->assertSame(123, Num::int('123', DecimalSeparator::COMMA));
-        $this->assertSame(1234567, Num::int('1 234 567,89', DecimalSeparator::COMMA));
-        $this->assertSame(1234567, Num::int('1.234.567,89', DecimalSeparator::COMMA));
-        $this->assertSame(1234567, Num::int('1\'234\'567,89', DecimalSeparator::COMMA));
-        $this->assertSame(12, Num::int('12,345', DecimalSeparator::COMMA));
-        $this->assertSame(0, Num::int('text', DecimalSeparator::COMMA));
-        $this->assertSame(0, Num::int(',12', DecimalSeparator::COMMA));
+        foreach ($testCases as $testCase) {
+            [$input, $expectedFloat, $expectedInt] = $testCase;
+            $this->assertSame($expectedFloat, Num::float($input, $separator));
+            $this->assertSame($expectedInt, Num::int($input, $separator));
+        }
     }
 }
